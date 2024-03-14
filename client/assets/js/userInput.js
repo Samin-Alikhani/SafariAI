e6ba57b1f4759840646e5d5a5c6ca5e25b866197
Matthew <feroshi1@gmail.com>
2024-03-14 18:59:02 -0500

    Index.html changes
    added JS to capture user info

---
+++ client/assets/js/userInput.js
@@ -0,0 +1,15 @@
+function Student(firstName, lastLetter, gradeLevel) {
+    this.firstName = firstName;
+    this.lastLetter = lastLetter;
+    this.gradeLevel = gradeLevel;
+}
+
+function getStudentInfo() {
+    let userFirstName = document.getElementById("firstName").value;
+    let userLastLetter = document.getElementById("lastLetter").value;
+    let userGradeLevel = document.querySelector('input[name="gradeLevel"]:checked').value;
+    let user = new Student(userFirstName, userLastLetter, userGradeLevel);
+    console.log(user);
+    sessionStorage.setItem("user", JSON.stringify(user));
+    location.replace("test.html");
+}
