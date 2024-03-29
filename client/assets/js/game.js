7355a7ce7b1248ca419c93c04ca7234fce29b4fb
Matthew <feroshi1@gmail.com>
2024-03-28 21:53:55 -0500

    Updated Game.html to recognize the webcam and show prediction results

--- client/assets/js/game.js
+++ client/assets/js/game.js
@@ -1,32 +1,50 @@
 const URL = "https://teachablemachine.withgoogle.com/models/kNZOxCcsh/";
+
+const webcamElement = document.getElementById('webcam');
+let stream, maxPredictions, model, labelContainer;
+
 //function to display webcam
-function init() {
+async function init() {
     const modelURL = URL + "model.json";
     const metadataURL = URL + "metadata.json";
-    var video = document.getElementById('webcam');
- 
-    navigator.mediaDevices.getUserMedia({ video: true })
-    .then(function (stream) {
-        video.srcObject = stream;
-        video.play();
-    })
-    .catch(function (err) {
-        console.log("An error occurred: " + err);
-    });
 
-    document.getElementById("webcam-container").appendChild(webcam.canvas);
+    //load the model and metadata
+    model = await tmImage.load(modelURL, metadataURL);
+    maxPredictions = model.getTotalClasses();
+
+    //setup webcam
+    if (navigator.mediaDevices.getUserMedia) {
+        navigator.mediaDevices.getUserMedia({ video: true })
+            .then((stream) => {
+                webcam.srcObject = stream;
+                webcam.play();
+                window.requestAnimationFrame(loop);
+            })
+            .catch((error) => {
+                console.error('Error accessing webcam:', error);
+            });
+    } else {
+        console.error('getUserMedia not supported in this browser.');
+    }
+
+    // append elements to the DOM
     labelContainer = document.getElementById("label-container");
-    for (let i = 0; i < maxPredictions; i++) { // and class labels
+    for (let i = 0; i < maxPredictions; i++) {
         labelContainer.appendChild(document.createElement("div"));
     }
 }
 
+async function loop() {
+    await predict(); // predict the result
+    window.requestAnimationFrame(loop);
+}
+
+// run the webcam image through the image model
 async function predict() {
     // predict can take in an image, video or canvas html element
-    const prediction = await model.predict(webcam.canvas);
+    const prediction = await model.predict(webcam);
     for (let i = 0; i < maxPredictions; i++) {
-        const classPrediction =
-            prediction[i].className + ": " + prediction[i].probability.toFixed(2);
+        const classPrediction = prediction[i].className + ": " + prediction[i].probability.toFixed(2);
         labelContainer.childNodes[i].innerHTML = classPrediction;
     }
 }
