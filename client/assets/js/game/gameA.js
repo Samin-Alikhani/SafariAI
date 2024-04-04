ac613b19bcd479c461ef7396392b4414c092c661
Matthew <feroshi1@gmail.com>
2024-04-04 14:15:19 -0500

    Changes to games

--- client/assets/js/game/gameA.js
+++ client/assets/js/game/gameA.js
@@ -2,7 +2,7 @@ const ModelA = "https://teachablemachine.withgoogle.com/models/W5TDNWZkL/";
 const gameRadio = document.getElementById('btnradio');
 const video = document.getElementById('video');
 
-let confidence;
+let confidence1, confidence2, confidence3;
 
 // Create a webcam capture
 navigator.mediaDevices.getUserMedia({ video: true }).then(stream => {
@@ -13,10 +13,19 @@ navigator.mediaDevices.getUserMedia({ video: true }).then(stream => {
   const loop = classifier => {
     classifier.classify().then(results => {
         //format decimal into percentage
-        confidence = results[0].confidence * 100;
+        confidence1 = results[0].confidence * 100;
+        confidence2 = results[1].confidence * 100;
+        confidence3 = results[2].confidence * 100;
 
-        result.innerText = results[0].label;
-        probability.innerText = confidence.toFixed(2) + "%";
+        result1.innerText = results[0].label;
+        probability1.innerText = confidence1.toFixed(2) + "%";
+
+        result2.innerText = results[1].label;
+        probability2.innerText = confidence2.toFixed(2) + "%";
+
+        result3.innerText = results[2].label;
+        probability3.innerText = confidence3.toFixed(2) + "%";
+
         loop(classifier); // Call again to create a loop
     });
   };
